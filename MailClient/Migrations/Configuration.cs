103965437e14d8485e38f8b569414dc085d1185f
Sebastian Marcinkowski <kontakt@sebastianmarcinkowski.pl>
2023-10-03 19:59:36 +0200

    Create database.

---
+++ MailClient/Migrations/Configuration.cs
@@ -0,0 +1,16 @@
+﻿namespace MailClient.Migrations
+{
+	using System.Data.Entity.Migrations;
+
+	internal sealed class Configuration : DbMigrationsConfiguration<MailClient.Models.ApplicationDbContext>
+	{
+		public Configuration()
+		{
+			AutomaticMigrationsEnabled = false;
+		}
+
+		protected override void Seed(MailClient.Models.ApplicationDbContext context)
+		{
+		}
+	}
+}
