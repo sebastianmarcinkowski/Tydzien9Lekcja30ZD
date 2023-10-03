c6ffe80d107fdde26cebec4cf5ba11de2d55e789
Sebastian Marcinkowski <kontakt@sebastianmarcinkowski.pl>
2023-10-03 21:51:54 +0200

    Create send mail form and add validation.

--- MailClient/Controllers/HomeController.cs
+++ MailClient/Controllers/HomeController.cs
@@ -1,11 +1,11 @@
-﻿using System;
+﻿using MailClient.Models.ViewModels;
 using System.Collections.Generic;
 using System.Linq;
-using System.Web;
 using System.Web.Mvc;
 
 namespace MailClient.Controllers
 {
+	[Authorize]
 	public class HomeController : Controller
 	{
 		public ActionResult Index()
@@ -13,6 +13,7 @@ namespace MailClient.Controllers
 			return View();
 		}
 
+		[AllowAnonymous]
 		public ActionResult About()
 		{
 			ViewBag.Message = "Your application description page.";
@@ -20,11 +21,41 @@ namespace MailClient.Controllers
 			return View();
 		}
 
+		[AllowAnonymous]
 		public ActionResult Contact()
 		{
 			ViewBag.Message = "Your contact page.";
 
 			return View();
+		}
+
+		public ActionResult SendMail(SendMailViewModel model)
+		{
+			if (!ModelState.IsValid)
+				return View("Index", model);
+
+			var recipients = ParseRecipments(model.To);
+
+			foreach (var recipment in recipients)
+			{
+				//SendMail...
+			}
+
+			//If success
+
+			TempData["message"] = Resources.Messages.SendMailConfirmation;
+
+			return RedirectToAction("Index");
+		}
+
+		private List<string> ParseRecipments(string to)
+		{
+			var outputList = new List<string>(to.Trim().Split(','));
+
+			return outputList
+				.Where(x => !string.IsNullOrWhiteSpace(x))
+				.Distinct()
+				.ToList();
 		}
 	}
 }
---
+++ MailClient/Models/ViewModels/SendMailViewModel.cs
@@ -0,0 +1,24 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace MailClient.Models.ViewModels
+{
+	public class SendMailViewModel
+	{
+
+		[Required(ErrorMessage = "Pole Nadawca jest wymagane.")]
+		[Display(Name = "Nadawca")]
+		public string SenderName { get; set; }
+
+		[Required(ErrorMessage = "Pole Odbiorca / Odbiorcy jest wymagane.")]
+		[Display(Name = "Odbiorca / Odbiorcy")]
+		public string To { get; set; }
+
+		[Required(ErrorMessage = "Pole Tytuł jest wymagane.")]
+		[Display(Name = "Tytuł")]
+		public string Title { get; set; }
+
+		[Required(ErrorMessage = "Pole Treść jest wymagane.")]
+		[Display(Name = "Treść")]
+		public string Content { get; set; }
+	}
+}
