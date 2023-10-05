8f7ba301f2a1e42e023e1ae5c578f8c2a09bd2d2
Sebastian Marcinkowski <kontakt@sebastianmarcinkowski.pl>
2023-10-05 20:21:25 +0200

    Add EmailSender and edit .gitignore.

---
+++ EmailSender/Extensions/StringExtensions.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Text.RegularExpressions;
+
+namespace EmailSender.Extensions
+{
+	public static class StringExtensions
+	{
+		public static string StripHtml(this string input)
+		{
+			return Regex.Replace(input, "<.*?>", String.Empty);
+		}
+	}
+}
